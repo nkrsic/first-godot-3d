d8c820c75f0da8ec02fbc94330ac0feb6f95d274
nkrsic <nikkrsic@gmail.com>
2024-06-03 21:05:01 -0400

    Add button to toggle cameras

--- Main.gd
+++ Main.gd
@@ -7,3 +7,7 @@ func _input(event):
 	if event.is_action_pressed("ui_focus_next"):
 		stationary_cam.current = not stationary_cam.current
 	
+
+
+func _on_Button_pressed():
+	stationary_cam.current = not stationary_cam.current # Replace with function body.
