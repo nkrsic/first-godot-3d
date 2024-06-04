997d676fffbe8cd3c7089b2b6f0ad9806818293e
nkrsic <nikkrsic@gmail.com>
2024-06-03 21:45:43 -0400

    Include all cameras in toggling logic + button

--- Main.gd
+++ Main.gd
@@ -1,13 +1,31 @@
 extends Node
 
 onready var stationary_cam = $StationaryCamera
+onready var ball_cam = $Ball/CameraRig/Camera
+onready var rotating_cam = $OuterCamera/Camera
+onready var skeleton_cam = $Skeleton/NeckPivot/Camera
 
+onready var cams = [
+	$StationaryCamera,
+	$Ball/CameraRig/Camera,
+	$OuterCamera/Camera,
+	$Skeleton/NeckPivot/Camera
+]
+
+onready var current_cam_idx = 0
+onready var current_cam = cams[current_cam_idx]
+
+func _ready():
+	current_cam.make_current()
+
+func toggleCameras():
+	current_cam_idx = (current_cam_idx + 1) % cams.size()
+	cams[current_cam_idx].make_current()
+	
 func _input(event):
 	print(event.as_text())
 	if event.is_action_pressed("ui_focus_next"):
-		stationary_cam.current = not stationary_cam.current
+		toggleCameras()
 	
-
-
 func _on_Button_pressed():
-	stationary_cam.current = not stationary_cam.current # Replace with function body.
+	toggleCameras()
